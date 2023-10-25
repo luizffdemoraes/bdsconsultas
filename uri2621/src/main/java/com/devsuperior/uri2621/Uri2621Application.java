9c970fe576a15d4aa50b5f7e27da7228e36f581a
Luiz Felipe <lffm1994@gmail.com>
2023-10-24 21:43:06 -0300

    05-12 URI 2621 Spring Boot SQL e JPQL

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,5 +1,7 @@
 package com.devsuperior.uri2621;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,18 +9,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
 
-	@Autowired
-	private ProductRepository repository;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Uri2621Application.class, args);
-	}
+    @Autowired
+    private ProductRepository repository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2621Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<ProductMinProjection> list = repository.search1(10, 20, "P");
+        List<ProductMinDTO> result1 = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+
+        System.out.println("\n *** RESULTADO SQL RAIZ:");
+        for (ProductMinDTO obj : result1) {
+            System.out.println(obj);
+        }
+        System.out.println("\n\n");
+
+
+        List<ProductMinDTO> result2 = repository.search2(10, 20, "P");
+
+        System.out.println("\n *** RESULTADO JPQL:");
+        for (ProductMinDTO obj : result2) {
+            System.out.println(obj);
+        }
+        System.out.println("\n\n");
+    }
 }
