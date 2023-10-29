15fc104d424ae1c93dbea029eef05537692f2785
Luiz Felipe <lffm1994@gmail.com>
2023-10-29 01:01:00 -0300

    05-21 URI 2990 Spring Boot SQL e JPQL

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +24,31 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		List<EmpregadoDeptProjection> list = repository.search1();
+		List<EmpregadoDeptDTO> result1 = list.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n *** RESULTADO SQL RAIZ NOT IN:");
+		for (EmpregadoDeptDTO obj : result1) {
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
+
+		List<EmpregadoDeptDTO> result2 = repository.search2();
+
+		System.out.println("\n *** RESULTADO JPQL:");
+		for (EmpregadoDeptDTO obj : result2) {
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
+
+		List<EmpregadoDeptProjection> list3 = repository.search1();
+		List<EmpregadoDeptDTO> result3 = list.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n *** RESULTADO SQL RAIZ LEFT JOIN:");
+		for (EmpregadoDeptDTO obj : result1) {
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
 	}
 }
